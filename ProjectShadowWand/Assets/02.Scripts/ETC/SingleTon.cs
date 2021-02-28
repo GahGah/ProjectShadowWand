a55519d05572edb86398b80c42ed9813adb82e94
JiWonHan <acsang12@gmail.com>
2021-02-28 20:17:24 +0900

    싱글톤 클래스와 InputManager추가

---
+++ ProjectShadowWand/Assets/02.Scripts/ETC/SingleTon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleTon<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+	protected static T instance = null;
+	public static T Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				//일반적인 Singleton 클래스는 게임오브젝트를 생성한다.
+				instance = new GameObject(typeof(T).ToString()).AddComponent<T>();
+				DontDestroyOnLoad(instance);
+			}
+			return instance;
+		}
+	}
+	//GameManager처럼 씬 게임오브젝트 컴포넌트로 이미 들어가있는 싱글턴들은
+	//무조건 Awake에서 Init함수를 통해 자기 자신을 instance로 지정하도록 한다.
+	protected virtual void Awake()
+	{
+		Init();
+	}
+
+	protected virtual void Init()
+	{
+		if (instance == null)
+		{
+			instance = this.gameObject.GetComponent<T>();
+			DontDestroyOnLoad(this);
+		}
+		else if (instance != this)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+
+}
---
+++ ProjectShadowWand/Assets/02.Scripts/Manager/InputManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class InputManager : MonoBehaviour
+{
+
+    public static InputManager instance = null;
+
+
+    public ButtonControl buttonUp;// = Keyboard.current.wKey;
+    public ButtonControl buttonDown;// = Keyboard.current.sKey;
+    public ButtonControl buttonLeft; //= Keyboard.current.aKey;
+    public ButtonControl buttonRight; //= Keyboard.current.dKey;
+    public ButtonControl buttonJump;// = Keyboard.current.spaceKey;
+    public ButtonControl buttonCtrl;// = Keyboard.current.ctrlKey;
+    public ButtonControl buttonPause;// = Keyboard.current.escapeKey;
+    public ButtonControl buttonCatch; //= Keyboard.current.leftShiftKey;
+
+    public ButtonControl buttonMouseLeft;// = Mouse.current.leftButton;
+    public Vector2Control buttonScroll;// = Mouse.current.scroll;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("이미 instance가 존재합니다." + this);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    private void Start()
+    {
+        SetButtonsDefaultKey();
+    }
+
+    //private void Update()
+    //{
+
+    //}
+    public void OnEnable()
+    {
+        Keyboard.current.onTextInput += OnTextInput;
+    }
+
+    public void OnDisable()
+    {
+        Keyboard.current.onTextInput -= OnTextInput;
+    }
+    public void OnTextInput(char c)
+    {
+        Debug.LogWarning(this.name + " : 입력된 키 [ " + c + " ]");
+    }
+
+    /// <summary>
+    /// 기본 조작키로 설정합니다. 
+    /// </summary>
+    public void SetButtonsDefaultKey()
+    {
+        buttonUp = Keyboard.current.wKey;
+        buttonLeft = Keyboard.current.aKey;
+        buttonDown = Keyboard.current.sKey;
+        buttonRight = Keyboard.current.dKey;
+        buttonJump = Keyboard.current.spaceKey;
+        buttonMouseLeft = Mouse.current.leftButton;
+        buttonCtrl = Keyboard.current.ctrlKey;
+        buttonPause = Keyboard.current.escapeKey;
+        buttonCatch = Keyboard.current.leftShiftKey;
+        buttonScroll = Mouse.current.scroll;
+    }
+
+    /// <summary>
+    /// 사용자가 설정한 조작키로 설정합니다.
+    /// </summary>
+    public void SetButtonsCustomKey()
+    {
+        //버튼1  = Keyboard.current.FindKeyOnCurrentKeyboardLayout("Json에서 읽어온 버튼1 스트링");
+    }
+
+}
