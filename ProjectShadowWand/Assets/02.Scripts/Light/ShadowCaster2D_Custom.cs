1a3e214943b83e6a9bb3072864b21063082e1bc3
zpdl0703 <47559493+zpdl0703@users.noreply.github.com>
2021-03-10 20:33:22 +0900

    custom ShadowCast2D R&D 중.

---
+++ ProjectShadowWand/Assets/02.Scripts/Light/ShadowCaster2D_Custom.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public enum ELight2DCategory
+{
+    Directional, //직선형
+    Radial //방사형
+};
+
+public delegate void Delegate_AddIntersectSegmentPoint(Vector3 point);
+
+public class ShadowCaster2D_Custom : MonoBehaviour
+{
+    public class SortComparer_Angle : IComparer<Vector2>
+    {
+        public int Compare(Vector2 first, Vector2 second)
+        {
+            float firstAngle = (float)((Mathf.Atan2(first.x, first.y) / Math.PI) * 180f);
+            float secondAngle = (float)((Mathf.Atan2(second.x, second.y) / Math.PI) * 180f);
+
+            firstAngle = firstAngle < 0 ? firstAngle + 360f : firstAngle;
+            secondAngle = secondAngle < 0 ? secondAngle + 360f : secondAngle;
+
+            if (firstAngle > secondAngle)
+                return -1;
+            else if (firstAngle == secondAngle)
+                return 0;
+            else
+                return -1;
+        }
+    }
+
+    private ShadowCaster2D_Manager SC2D_Manager;
+
+    [SerializeField] private ELight2DCategory lightCategory = ELight2DCategory.Radial;
+    [SerializeField] private int rayCount = 50;
+
+    public Delegate_AddIntersectSegmentPoint AddIntersectSegmentPoint;              // 함수 대리자
+    public List<Vector2> intersectPoint = new List<Vector2>();
+
+    private Light2D light2d = null;
+    private float unitRot;
+    private float distance = 0.0f;
+
+    private void Awake()
+    {
+        light2d = this.GetComponent<Light2D>();
+        
+        if (light2d == null)
+            Debug.Log("!!! light2D is null !!!");
+
+    }
+
+    void Start()
+    {
+        SC2D_Manager = ShadowCaster2D_Manager.Instance;
+        if (SC2D_Manager == null)
+            Debug.Log("!!! SC2D_Manager is null !!!");
+
+        SC2D_Manager.AddShadowLightObj(this.gameObject);
+
+        switch (lightCategory)
+        {
+            case ELight2DCategory.Directional:
+                Init_Directinal();
+                break;
+
+            case ELight2DCategory.Radial:
+                Init_Redial();
+                break;
+        }
+    }
+
+    /*-------------------------------------------------*/
+
+    void Init_Directinal()
+    {
+        //pass
+    }
+
+    void Init_Redial()
+    {
+        AddIntersectSegmentPoint = new Delegate_AddIntersectSegmentPoint(AddIntersectSegmentPoint_Radial);
+
+        distance = light2d.pointLightOuterRadius;
+        unitRot = 360 / (float)rayCount;
+
+        Debug.Log("unitRot: " + unitRot);
+        Debug.Log("distance: " + distance);
+    }
+
+    public void SortIntersectPoint()
+    {
+        var convPosLocal = new Converter<Vector2, Vector2>(x => x - (Vector2)this.transform.position);
+        var convPosWorld = new Converter<Vector2, Vector2>(x => x + (Vector2)this.transform.position);
+
+        intersectPoint.ConvertAll<Vector2>(convPosLocal);
+        intersectPoint.Sort(new SortComparer_Angle());
+        intersectPoint.ConvertAll<Vector2>(convPosWorld);
+    }
+
+    public void ClearIntersectPoint()
+    {
+        intersectPoint.Clear();
+    }
+
+    //
+    public void AddIntersectSegmentPoint_Radial(Vector3 point)
+    {
+        Vector2 pointV2 = point;
+        Vector2 direction = (pointV2 - new Vector2(this.transform.position.x, this.transform.position.y)).normalized;   // this에서 point로 향하는 벡터 + 정규화.
+        distance = light2d.pointLightOuterRadius;
+
+        // 1차 hit.
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, direction, distance, (int)SC2D_Manager.includeLayer);
+        if (hit)
+        {
+            Debug.DrawLine(this.transform.position, hit.point, Color.blue);
+            intersectPoint.Add(hit.point);
+
+            // 2차 hit (확장)
+            // hit한 곳이 모서리일 경우 확장시켜줌.
+            if(hit.point == pointV2)
+            {
+                RaycastHit2D extendedRayhit = Physics2D.Raycast(hit.point + direction*0.1f, direction, Vector2.Distance(hit.point, (Vector2)this.transform.position + (direction * distance)), (int)SC2D_Manager.includeLayer);
+                if (extendedRayhit)
+                {
+                    Debug.DrawLine(hit.point + direction * 0.1f, extendedRayhit.point, Color.cyan);
+                    intersectPoint.Add(extendedRayhit.point);
+                }
+                else
+                {
+                    Debug.DrawLine(hit.point, (Vector2)this.transform.position + (direction * distance), Color.magenta);
+                }
+            }
+        }
+        else
+        {
+            // 히트하지 않았는데 둘 사이의 거리가 범위 안에 있다 -> raycast의 오차이므로 보정해줌.
+            if (Vector2.Distance((Vector2)this.transform.position, pointV2) <= distance)
+            {
+                Debug.DrawLine(this.transform.position, pointV2, Color.blue);
+                intersectPoint.Add(pointV2);
+
+                RaycastHit2D extendedRayhit = Physics2D.Raycast(pointV2 + direction * 0.1f, direction, Vector2.Distance(pointV2, (Vector2)this.transform.position + (direction * distance)), (int)SC2D_Manager.includeLayer);
+                if (extendedRayhit)
+                {
+                    Debug.DrawLine(pointV2 + direction * 0.1f, extendedRayhit.point, Color.cyan);
+                    intersectPoint.Add(extendedRayhit.point);
+                }
+                else
+                {
+                    Debug.DrawLine(pointV2, (Vector2)this.transform.position + (direction * distance), Color.magenta);
+                }
+            }
+            else
+            {
+                Debug.DrawRay(this.transform.position, direction * distance, Color.red);
+            }
+        }
+    }
+
+    /*
+    void FindIntersectSegment_Directional()
+    {
+
+    }
+
+    void FindIntersectSegment_Radial()
+    {
+        Vector2 rayDirection = Vector2.zero;
+
+        for (int i = 0; i<rayCount; ++i)
+        {
+            //rayDirection = unitQRot * rayDirection;
+            //Debug.Log("rayDirection: " + rayDirection);
+
+            rayDirection.x = Mathf.Cos(unitRot*i);
+            rayDirection.y = Mathf.Sin(unitRot*i);
+
+            distance = light2d.pointLightOuterRadius;
+
+            Debug.Log("angle: " + unitRot * i);
+            Debug.DrawRay(this.transform.position, rayDirection*distance, Color.red);
+
+            RaycastHit2D hit;
+            if (hit = Physics2D.Raycast(this.transform.position, rayDirection, distance, (int)SC2D_Manager.includeLayer))
+            {
+                Debug.DrawLine(hit.point, hit.point + new Vector2(rayDirection.x, rayDirection.y), Color.blue);
+            }
+        }
+    }
+    */
+}
---
+++ ProjectShadowWand/Assets/02.Scripts/Light/ShadowCaster2D_Manager.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Flags]
+public enum ELayerCategory
+{
+    Default_0 = 1 << 0,
+    TransparentFX_1 = 1 << 1,
+    IgnoreRaycast_2 = 1 << 2,
+    _3 = 1 << 3,
+    Water_4 = 1 << 4,
+    UI_5 = 1 << 5,
+    _6 = 1 << 6,
+    _7 = 1 << 7,
+    Monster_8 = 1 << 8,
+    Ground_9 = 1 << 9,
+    _10 = 1 << 10,
+};
+
+
+public class ShadowCaster2D_Manager : MonoBehaviour
+{
+    public static ShadowCaster2D_Manager Instance;
+
+    public ELayerCategory includeLayer = ELayerCategory.Default_0;
+    [SerializeField] private GameObject[] includeObjects = null;
+    [SerializeField] private PolygonCollider2D[] includeObjectsPolygonCol2D = null;
+    [SerializeField] private CompositeCollider2D[] includeObjectCompositeCol2D = null;
+
+    private List<ShadowCaster2D_Custom> shadowLightsSC2D = new List<ShadowCaster2D_Custom>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            GameObject.DontDestroyOnLoad(this.gameObject); // 씬 로딩을 할 때(옮겨다닐 때) 지우지마라 
+        }
+    }
+
+    private void Start()
+    {
+        SetIncludeObjects();
+    }
+
+    private void Update()
+    {
+        ClearAllIntersectSegmentPoint();
+        SetAllIntersectSegmentPoint();
+    }
+
+    /*-----------------------------------------------------*/
+
+    // 선택한 레이어에 포함된 게임 오브젝트와, 갖고 있는 PolygonCollider2D를 배열로 저장. [ O(1)의 접근을 위함. ]
+    public void SetIncludeObjects()
+    {
+        if (includeObjects != null)
+            Array.Clear(includeObjects, 0, includeObjects.Length);
+        if (includeObjectsPolygonCol2D != null)
+            Array.Clear(includeObjectsPolygonCol2D, 0, includeObjectsPolygonCol2D.Length);
+        if (includeObjectCompositeCol2D != null)
+            Array.Clear(includeObjectCompositeCol2D, 0, includeObjectCompositeCol2D.Length);
+
+        GameObject[] temp_allGameObejcts = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];    // 씬 내의 모든 게임 오브젝트를 가져옴.
+        List<GameObject> temp_includeObjectList = new List<GameObject>();
+        List<PolygonCollider2D> temp_includeObjectPolygonCol2DList = new List<PolygonCollider2D>();
+        List<CompositeCollider2D> temp_includeObjectCompositeCol2DList = new List<CompositeCollider2D>();
+
+        Debug.Log(temp_allGameObejcts);
+
+        foreach(GameObject Obj in temp_allGameObejcts)
+        {
+            if (includeLayer.HasFlag((ELayerCategory)(1<<Obj.layer)))
+            {
+                temp_includeObjectList.Add(Obj);
+
+                PolygonCollider2D temp_polygonCol2DComponent = Obj.GetComponent<PolygonCollider2D>();
+                if (temp_polygonCol2DComponent != null)
+                    temp_includeObjectPolygonCol2DList.Add(temp_polygonCol2DComponent);
+
+                CompositeCollider2D temp_compositeCol2DComponent = Obj.GetComponent<CompositeCollider2D>();
+                if (temp_compositeCol2DComponent != null)
+                    temp_includeObjectCompositeCol2DList.Add(temp_compositeCol2DComponent);
+
+            }
+        }
+
+        includeObjects = new GameObject[temp_includeObjectList.Count];
+        includeObjects = temp_includeObjectList.ToArray();
+
+        includeObjectsPolygonCol2D = new PolygonCollider2D[temp_includeObjectPolygonCol2DList.Count];
+        includeObjectsPolygonCol2D = temp_includeObjectPolygonCol2DList.ToArray();
+
+        includeObjectCompositeCol2D = new CompositeCollider2D[temp_includeObjectCompositeCol2DList.Count];
+        includeObjectCompositeCol2D = temp_includeObjectCompositeCol2DList.ToArray();
+    }
+
+    // 그림자를 그리는 빛 리스트에 obj를 추가.
+    public void AddShadowLightObj(GameObject obj)
+    {
+        shadowLightsSC2D.Add(obj.GetComponent<ShadowCaster2D_Custom>());
+    }
+
+
+    public void ClearAllIntersectSegmentPoint()
+    {
+        for (int i =0; i<shadowLightsSC2D.Count; ++i)
+        {
+            shadowLightsSC2D[i].ClearIntersectPoint();
+        }
+    }
+
+    public void SetAllIntersectSegmentPoint()
+    {
+        // ToDo:
+        // 1. 화면(+ a 범위) 내에 있는 오브젝트에 대해서만 for문.
+        // 2. 화면(+ a 범위) 내에 있는 vertex에 대해서만 함수 넘겨줌.
+        // 을 해주어야 함.
+
+        // Polygon Col 2D를 갖고 있는 오브젝트에 대해.
+        for(int i = 0; i<includeObjectsPolygonCol2D.Length; ++i)
+        {
+            Vector2[] polyCol2DVertex = includeObjectsPolygonCol2D[i].points;
+
+            for (int j = 0; j < polyCol2DVertex.Length; ++j)
+            {
+                for (int k = 0; k < shadowLightsSC2D.Count; ++k)
+                {
+                    shadowLightsSC2D[k].AddIntersectSegmentPoint(
+                        includeObjectsPolygonCol2D[i].transform.TransformPoint(polyCol2DVertex[j])
+                        );
+                }
+            }
+        }
+
+        // Composite Col 2D를 갖고 있는 오브젝트에 대해.
+        for (int i = 0; i < includeObjectCompositeCol2D.Length; ++i)
+        {
+            Vector2[] compositeCol2DVertex = new Vector2[0];
+
+            // Composite Col 2D의 정점을 배열에 넣음.
+            for (int j = 0; j < includeObjectCompositeCol2D[i].pathCount; ++j)
+            {
+                compositeCol2DVertex = new Vector2[includeObjectCompositeCol2D[i].GetPathPointCount(j)];
+                includeObjectCompositeCol2D[i].GetPath(j, compositeCol2DVertex);
+            }
+
+            for (int j = 0; j < compositeCol2DVertex.Length; ++j)
+            {
+                for (int k = 0; k < shadowLightsSC2D.Count; ++k)
+                {
+                    shadowLightsSC2D[k].AddIntersectSegmentPoint(
+                        includeObjectCompositeCol2D[i].transform.TransformPoint(compositeCol2DVertex[j])
+                        );
+                }
+            }
+        }
+    }
+
+    public void CreateMesh()
+    {
+        // 잠시 고민. 매쉬를 생성하는 것 까지는 좋은데, 블렌딩이 가능할까?
+        // uv나 그런것도 직접 펴야하는데.
+
+        for (int i =0; i<shadowLightsSC2D.Count; ++i)
+        {
+            shadowLightsSC2D[i].SortIntersectPoint();
+        }
+    }
+}
