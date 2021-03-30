4e8951747ffd10952dc0f106467828e331a8686c
JiWonHan <acsang12@gmail.com>
2021-03-30 23:28:10 +0900

    기존에 한거 푸시

--- ProjectShadowWand/Assets/02.Scripts/Dummy/TestShadowTextureLoad.cs
+++ ProjectShadowWand/Assets/02.Scripts/Dummy/TestShadowTextureLoad.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Rendering.Universal;
 
 public class TestShadowTextureLoad : MonoBehaviour
 {
-    RenderTexture testRenderTexture;
+    public RawImage rawImage;
+    public RenderTexture testRenderTexture;
     // Start is called before the first frame update
     void Start()
     {
         //bits = 0,16,24라는데...24비트로 했을 때만 스텐실 버퍼가 생긴다함
-        testRenderTexture = ShadowUtils.GetTemporaryShadowTexture(500, 500, 24);
 
+        //rawImage.texture = testRenderTexture;
+        //testRenderTexture = ShadowUtils.GetTemporaryShadowTexture(1920, 1080, 24);
 
+        rawImage.texture = testRenderTexture;
+        testRenderTexture = ShadowUtils.GetTemporaryShadowTexture(1920, 1080, 24);
+        RenderTexture.ReleaseTemporary(testRenderTexture);
     }
 
     // Update is called once per frame
