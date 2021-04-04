9142d28c1f7e163b07709d35cf8d0b520ff100b1
JiWonHan <acsang12@gmail.com>
2021-04-05 00:56:18 +0900

    토네이도 작업 중

---
+++ ProjectShadowWand/Assets/02.Scripts/WeatherInteractionObject/Tornados/TornadoSecond.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoSecond : MonoBehaviour
+{
+    /// <summary>
+    /// 플레이어와 기타 오브젝트와 충돌하지 않는 리지드 바디. (중요)
+    /// </summary>
+    public Rigidbody2D tornadoRigidBody; 
+    public Collider2D tornadoTrigger;
+
+}
