44b176923e9afe0b3766815d66483c54ba394a17
zpdl0703 <47559493+zpdl0703@users.noreply.github.com>
2021-04-24 19:51:53 +0900

    VFX -불 임시 파일

---
+++ ProjectShadowWand/Assets/VFX TEMP FILES/Scripts/VFX/Light/FlickerLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour
+{
+    private new Light light;
+    [SerializeField] private Vector2 intensityRange = new Vector2(1, 1);
+
+    private float defaultIntensity;
+    private void Awake()
+    {
+        light = this.GetComponent<Light>();
+        defaultIntensity = light.intensity;
+    }
+
+    void Update()
+    {
+        light.intensity = defaultIntensity * Random.Range(intensityRange.x, intensityRange.y);
+    }
+}
