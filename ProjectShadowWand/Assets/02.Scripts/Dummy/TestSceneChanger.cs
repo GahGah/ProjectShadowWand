fbe0b5af705ab1721395834816adb8ed96166aa2
JiWonHan <acsang12@gmail.com>
2021-04-27 01:21:11 +0900

    타워크레인 로프 적용 중

--- ProjectShadowWand/Assets/02.Scripts/Dummy/TestSceneChanger.cs
+++ ProjectShadowWand/Assets/02.Scripts/Dummy/TestSceneChanger.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TestSceneChanger : MonoBehaviour
 {
     public string moveSceneName;
+
+    public Image goBlackImage;
+
+    private void Start()
+    {
+        goBlackImage.gameObject.SetActive(false);
+    }
     public void ButtonGoPlayerScene()
     {
         SceneManager.LoadScene(moveSceneName);
     }
 
+
+    public void PlayerDie_SceneReload()
+    {
+        StartCoroutine(ProcessDie());
+    }
+
     public void Test()
     {
         Screen.fullScreen = false;
+    }
+
+    IEnumerator ProcessDie()
+    {
+        var timer = 0f;
+        var maxTime = 2f;
+        var progress = 0f;
+        var twoMyeong = new Color32(0, 0, 0, 0);
+        var black = new Color32(0, 0, 0, 255);
+
+        yield return new WaitForSeconds(2f);
+
+        goBlackImage.gameObject.SetActive(true);
+        while (progress < 1f)
+        {
+            timer += Time.deltaTime;
+            progress = timer / maxTime;
+            Debug.Log("Progress : "+ progress);
+
+            goBlackImage.color = Color32.Lerp(twoMyeong, black, progress);
+            yield return new WaitForEndOfFrame();
+        }
+        SceneManager.LoadScene(moveSceneName);
+
     }
 }
