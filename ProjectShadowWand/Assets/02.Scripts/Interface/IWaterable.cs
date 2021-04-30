1679f45be9ebc9bf1c6fbbeff903d999e2602273
JiWonHan <acsang12@gmail.com>
2021-04-30 16:23:46 +0900

    자라는 오브젝트 / 물 닿음 인터페이스

---
+++ ProjectShadowWand/Assets/02.Scripts/Interface/IWaterable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWaterable
+{
+
+    /// <summary>
+    /// 물에 맞았을 때
+    /// </summary>
+    public void OnWater();
+}
