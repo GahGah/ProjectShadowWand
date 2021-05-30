97a634be5e0b1f6444646057ef9e55cce40919e3
JiWonHan <acsang12@gmail.com>
2021-05-30 21:16:30 +0900

    Stage_00 배치 준비

--- ProjectShadowWand/Assets/02.Scripts/Manager/StageManager.cs
+++ ProjectShadowWand/Assets/02.Scripts/Manager/StageManager.cs
@@ -84,7 +84,7 @@ public class StageManager : Manager<StageManager>
         // UpdateStageName();
 
         trashList = new List<InteractMoveObject>();
-        UpdateStageName();
+        UpdateStageName_TEST();
     }
 
     private void UpdateStageNameList()
