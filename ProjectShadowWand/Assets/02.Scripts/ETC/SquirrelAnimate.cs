ca7bf38cf4a65a6572c69b1bb4b4b4bf2655c035
JiWonHan <acsang12@gmail.com>
2021-06-04 21:46:37 +0900

    다람쥐 고개 돌림

---
+++ ProjectShadowWand/Assets/02.Scripts/ETC/SquirrelAnimate.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquirrelAnimate : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private int animatorLookBool;
+    private void Awake()
+    {
+        animatorLookBool = Animator.StringToHash("Look");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(ProcessPlayerMoveCheck());
+    }
+
+
+    private IEnumerator ProcessPlayerMoveCheck()
+    {
+
+        //땅에 닿을 때 까지 기다림
+        while (true)
+        {
+            if (PlayerController.Instance.isGrounded == true)
+            {
+                break;
+            }
+
+            yield return YieldInstructionCache.WaitForFixedUpdate;
+
+        }
+
+        var startPos = PlayerController.Instance.transform.position;
+        var playerPos = PlayerController.Instance.transform;
+
+        //x축 위치 이동을 할 때까지 기다림
+        while (true)
+        {
+            if (playerPos.position.x != startPos.x)
+            {
+                break;
+            }
+
+            yield return YieldInstructionCache.WaitForFixedUpdate;
+        }
+
+        animator.SetBool(animatorLookBool,true);
+    }
+}
